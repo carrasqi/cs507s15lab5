9a3cb4ecf1660e143822ea9ad7efeb8f9a8d4fdd
carrasqi <imani.carrasquillo2016@gmail.com>
2015-02-21 16:45:14 -0500

    timey wimey stuff

--- Hello.java
+++ Hello.java
@@ -1,19 +1,15 @@
-public class Hello { 
-   public static void main(String[] args) { 
-      System.out.println("Hello, Java!");
-      System.out.println("We are the bomb!");
-      System.out.println("Jimmy, Imani, and Shannon");
-      //update by Shannon
-<<<<<<< HEAD
-      System.out.println("Update 1 lab 5 ");
-      //Update by Jimmy
-      System.out.println("Update 2: This lab took way too long.");
-      //update by Imani
-      System.out.println("Update 3: I second that motion");
-=======
-      System.out.println("Update 1 lab 5");
-      //Update by Jimmy
-      System.out.println("Update 2: This lab took way too long.");
->>>>>>> 59133cbad8625f9ada686636ccf879e3dad53572
-   }
+public class Hello
+{
+    public static void main(String[]args)
+    {
+	System.out.println("We are the bomb!");
+	System.out.println("Jimmy, Imani, and Shanon");
+	//update by Shannon
+	System.out.println("Update 1 lab 5");
+	//Update by Jimmy
+	System.out.println("Update 2: This lab took way too long.");
+	//update by Imani
+	System.out.println("Update 3: I second that motion");
+ 
+    }
 }
