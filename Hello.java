08a03330d23b50c6e6c0f4ce5f764626c72d5fae
carrasqi <imani.carrasquillo2016@gmail.com>
2015-02-21 16:22:32 -0500

    timey, wimey, stuff

--- Hello.java
+++ Hello.java
@@ -3,6 +3,11 @@ public class Hello {
       System.out.println("Hello, Java!");
       System.out.println("We are the bomb!");
       System.out.println("Jimmy, Imani, and Shannon");
-      System.out.println("Update 1 lab 5")
+      //update by Shannon
+      System.out.println("Update 1 lab 5 ");
+      //Update by Jimmy
+      System.out.println("Update 2: This lab took way too long.");
+      //update by Imani
+      System.out.println("Update 3: I second that motion");
    }
 }
